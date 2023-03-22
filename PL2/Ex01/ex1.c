476501a50f4bd6fb0cb0a896fdcea1390f1b5ea3
N0R70N <1210824@isep.ipp.pt>
2023-03-22 14:25:01 +0000

    Ex1 PL2

---
+++ PL2/Ex01/ex1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int main(void){
+
+	int fd[2];
+	pid_t pid;
+	
+    if(pipe(fd) == -1){
+		perror("Pipe failed");
+		return 1;
+	}
+
+	pid = fork();
+	if(pid > 0){
+		close(fd[0]);
+
+		pid_t myPid = getpid();
+		printf("Parent wrote: %d\n", myPid);
+		write(fd[1],&myPid,sizeof(pid_t));
+		
+        close(fd[1]);
+
+	} else{
+		close(fd[1]);
+		
+        pid_t parentPID;
+		read(fd[0], &parentPID, sizeof(pid_t));
+		printf("Child read: %d\n", parentPID);
+		
+        close(fd[0]);
+		}
+	
+}
