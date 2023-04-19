d4d8e3d2eaf9ec3c3ac0a46939782ec2a3693156
NunoBarbosa20 <1210822@isep.ipp.pt>
2023-04-19 16:03:34 +0100

    exeficcio 10 fixed

--- PL3/Ex10/ex10.c
+++ PL3/Ex10/ex10.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
+#include <unistd.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
 #include <fcntl.h>
-#include <time.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <string.h>
 #include <errno.h>
+#include <time.h>
 
-#define shm_name "/shm_ex10"
+#define shm_name "/shm_ex9"
+#define AMOUNT 30
+#define BUFFER_SIZE 10
+#define BUFFER_MASK (BUFFER_SIZE - 1)
 
-typedef struct{
-	int nums[10];
-	int itemsLeft;
-} ex10_type;
+typedef struct circular_buffer{
+    int buffer[BUFFER_SIZE];
+    int head;
+    int tail;
+    int count;
+} circular_buffer;
 
-int main(void) {
-	printf("Producing");
 
-	int data_size = sizeof(ex10_type);
+int main() {
+    int buffer_size = sizeof(circular_buffer);
     int fd = shm_open(shm_name, O_RDWR, S_IRUSR|S_IWUSR);
     if (fd < 0) {
         if (errno == ENOENT) {
@@ -30,7 +35,7 @@ int main(void) {
                 perror("shm_open");
                 exit(1);
             }
-            if (ftruncate(fd, data_size) < 0) {
+            if (ftruncate(fd, buffer_size) < 0) {
                 perror("ftruncate");
                 exit(1);
             }
@@ -39,86 +44,85 @@ int main(void) {
             exit(1);
         }
     }
-   ex10_type *sharedMemory = (ex10_type*) mmap(NULL, data_size, PROT_READ|PROT_WRITE,MAP_SHARED, fd, 0);
-    if (sharedMemory == MAP_FAILED) {
+    circular_buffer *circBuf = mmap(NULL, buffer_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (circBuf == MAP_FAILED) {
         perror("mmap");
         exit(EXIT_FAILURE);
     }
-    
-	int pipe1[2];
-	int pipe2[2];
-	pid_t pid = fork();
-	if (pid < 0) {
+	
+	int producer_pipe[2];
+	int consumer_pipe[2];
+	
+	if (pipe(producer_pipe) < 0 || pipe(consumer_pipe) < 0) {
+        perror("pipe");
+        exit(1);
+    }
+
+    circBuf->head = 0;
+    circBuf->tail = 0;
+    circBuf->count = 0;
+
+
+
+    pid_t pid = fork();
+    if (pid < 0) {
         perror("fork failed");
         exit(1);
+    } else if (pid == 0) {
+		
+		close(producer_pipe[0]);
+		close(consumer_pipe[1]);
+		
+        for (int i = 1; i <= AMOUNT; i++) {
+			int value;
+			read(producer_pipe[0],&value,sizeof(int));
+			while (circBuf -> count == BUFFER_SIZE);
+            circBuf->buffer[circBuf->tail] = i;
+            circBuf->tail = (circBuf->tail + 1) % BUFFER_SIZE;
+            circBuf->count++;
+            write(consumer_pipe[1],&value,sizeof(int));
+
         }
-        
-	int writing;
-	printf("Producing");
 
-	if(pid>0){
-		//producer
-		close(pipe1[0]);
-		close(pipe2[1]);
-		writing=0;
-		int fd = shm_open(shm_name, O_CREAT|O_RDWR, S_IRUSR|S_IWUSR);
-		ftruncate (fd, data_size);
-		ex10_type *sharedMemory = (ex10_type*) mmap(NULL, data_size, PROT_READ|PROT_WRITE,MAP_SHARED, fd, 0);
-		sharedMemory->itemsLeft = 30;
-		
-		int newNumber=0;
-		while(sharedMemory->itemsLeft>0){
+        exit(0);
+    }
+    
+    close(producer_pipe[1]);
+    close(consumer_pipe[0]);
+    
+    
+    for (int i = 0; i < AMOUNT; i++) {
+		int value;
+		write(producer_pipe[1], &value, sizeof(int));
+        while (circBuf->count == 0);
+        read(consumer_pipe[0], &value, sizeof(int));
+        printf("Consumer: %d\n", value);
+        circBuf->head = (circBuf->head + 1) % BUFFER_SIZE;
+        circBuf->count--;
+    }
 
-			while(writing==1){
 
-				read(pipe2[0],&writing,sizeof(int));
-			}
-			for(int i=0;i<10;i++){
-				sharedMemory->nums[i]=newNumber;
-				newNumber++;
-				sharedMemory->itemsLeft--;
-			}
-			writing=1;
-			
-			write(pipe1[1],&writing,sizeof(int));
+    
 
-		}
-		close(pipe1[1]);
-		close(pipe2[0]);
-		
-	}else{
-		//consumer
-		close(pipe1[1]);
-		close(pipe2[0]);
-		writing = 0;
-		int fd = shm_open(shm_name, O_RDWR, S_IRUSR|S_IWUSR);
-		ftruncate (fd, data_size);
-		ex10_type *sharedMemory = (ex10_type*) mmap(NULL, data_size, PROT_READ|PROT_WRITE,MAP_SHARED, fd, 0);
-		while(1){
-			while(writing!=1){
-	
-				read(pipe1[0],&writing,sizeof(int));
-			}
-			for(int i=0;i<10;i++){
-				printf("%d\n",sharedMemory->nums[i]);
-			}
-			
-			writing = 0;
-			write(pipe2[1],&writing,sizeof(int));
-			printf("printed 10 nums, %d remaining\n",sharedMemory->itemsLeft);
-			if(sharedMemory->itemsLeft==0)
-				break;
-		}
-		close(pipe1[0]);
-		close(pipe2[1]);
-		exit(1);
-	}
-		int status;
-		wait(&status);
 
-	
-	
-	shm_unlink(shm_name);
+    waitpid(pid,NULL,0);
 
-	return 0; 
+
+    if (munmap(circBuf, buffer_size) == -1) {
+        perror("munmap");
+        exit(EXIT_FAILURE);
+    }
+
+    if (fd >= 0 && close(fd) < 0) {
+        perror("close");
+        exit(1);
+    }
+
+
+    if (shm_unlink(shm_name) < 0) {
+        perror("shm_unlink");
+        exit(1);
+    }
+
+    return 0;
 }
